cbe047883d6a94e824b3260a44cdeb8335727ff0
Mahaboob Pasha <60670917+Mahaboobpash@users.noreply.github.com>
2020-04-27 10:28:49 +0530

    Update Demo.java

--- src/simple/Demo.java
+++ src/simple/Demo.java
@@ -3,5 +3,6 @@ package simple;
 public class Demo {
 public static void main(String[] args) {
 	System.out.println("Git from Command prompt");
+	System.out.println("Pull from remote repo to local repo");
 }
 }
